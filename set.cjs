9d6e09a10ef5753d6dfb747fce0902b3f4abdfc9
Javaboymr2k <javaboymr2k@gmail.com>
2024-10-23 19:32:41 +0200

    Update set.cjs

--- set.cjs
+++ set.cjs
@@ -1,9 +1,9 @@
-// JAVA BOY MR2K TECH SUPPORT //
+// JAVA BOY MR2K TECH //
 const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Gifted~8vr054ZQ",
+  SESSION_ID: process.env.SESSION_ID || "Gifted~XptfpJEu",
   HEROKU_API_TOKEN: process.env.HEROKU_API_TOKEN || "HRKU-0a1fe665-8a35-42e8-b7ad-761f25ffe195",
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
   KOYEB_API_TOKEN: process.env.KOYEB_API_TOKEN || "",
